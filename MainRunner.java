19bbe96a17e82731c00b08b75dd9b37bab287ef6
Karthik Yagnamurthy <karthik.yagnamurthy@gmail.com>
2022-03-16 18:37:25 -0400

    perfect square prints!

--- MainRunner.java
+++ MainRunner.java
@@ -1,5 +1,25 @@
+import java.lang.Math;
+import java.util.*;
+
+interface Squared{
+    public boolean perfectSquare(double num);
+}
 public class MainRunner{
-    public static void main(String[] args){
-        
+ public static void main(String[] args){
+       Squared sq = num ->{
+           double root = Math.pow(num, 0.5);
+           String s = ''''+root;
+
+           if(num<0){
+               return false;
+           }
+           else if((s.substring(s.indexOf(".")+1)).equals("0")){
+               return true;
+           }
+           else{
+               return false;
+           }
+       };
+       System.out.println(sq.perfectSquare(1.0))
     }
 }
