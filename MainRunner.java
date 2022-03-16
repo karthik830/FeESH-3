1fc3de8896c0824315a3c0f654c7dd5ac01f0270
Kapil <kapilr3500@gmail.com>
2022-03-16 18:38:50 -0400

    cool

--- MainRunner.java
+++ MainRunner.java
@@ -1,5 +1,13 @@
 public class MainRunner{
     public static void main(String[] args){
-        
+        Quotient q = (one, two) ->{
+            float q = one/two;
+            System.out.printf("Quotient: %.3f", q);
+        };
+        q.printQuotient(10, 5);
+    }
+    public Interface Quotient
+    {
+        public void printQuotient(float one, float two);
     }
 }
